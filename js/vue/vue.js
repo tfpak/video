aab1b6cda9c41036ff381a3ea73c7b939f7c9b5d
tfpak <48423431+tfpak@users.noreply.github.com>
2019-03-20 13:22:56 +0800

    Update vue.js

--- js/vue/vue.js
+++ js/vue/vue.js
@@ -21,8 +21,8 @@ alert("url的值为空，要加上m3u8格式的视频网址（例如）：/?url=
   var jul = 'https://tfpak.github.io/v/m3u8.html?url=';
   var vul = 'https://video.letv-cdn.com/20171124/Y2ne5XAt/index.m3u8'
   var gul ='./gul/yingq/'
+  var ymul ='https://tfpak.github.io/video/'
 $(document).ready(function(){
   $('#barframe').attr('src',jul+vid);
-  });
   $('#ba-guang').attr('src',ymul+gul);
   });
